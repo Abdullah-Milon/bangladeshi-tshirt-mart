ca0f030328e22c0df9bec28e925c94871c53ddf6
Abdullah-Milon <almilonabdullah80@gmail.com>
2023-04-12 14:19:51 +0600

    use react hook name useContext

--- src/components/Grandpa/Grandpa.jsx
+++ src/components/Grandpa/Grandpa.jsx
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Uncle from '../Uncle/Uncle';
 import Anty from '../Anty/Anty';
 import Father from '../Father/Father';
 import './Grandpa.css'
+import { createContext } from 'react';
+
+export const RingContext = createContext('gold');
+export const MoneyContext = createContext(0)
 
 const Grandpa = () => {
     const ring = 'diamond';
+    const [money, setMoney] = useState(0);
     return (
         <div className='grandpa'>
             <h2>This is GrandPa</h2>
-            <section className='flex'>
-                <Father ring = {ring}></Father>
-                <Uncle></Uncle>
-                <Anty ring= {ring}></Anty>
-            </section>
+            <p>Has money: {money}</p>
+            <MoneyContext.Provider value={[money, setMoney]}>
+                <RingContext.Provider value='golden Ring'>
+                    <section className='flex'>
+                        <Father ring={ring}></Father>
+                        <Uncle></Uncle>
+                        <Anty ring={ring}></Anty>
+                    </section>
+                </RingContext.Provider>
+            </MoneyContext.Provider>
         </div>
     );
 };
 
 export default Grandpa;
+
+/**
+ * 1. Create a context and export
+ * 2. Create a provider and pass a value
+ * 3. use useContext to recive  
+ *  **/
